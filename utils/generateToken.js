84a2feb4b719ab534699c2ae8d3eb12955e92712
unknown <michaelfeleke13@gmail.com>
2023-12-31 11:12:03 -0800

    generate token same site tone

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -11,7 +11,8 @@ const generateToken = (res,userId) => {
     
 
     res.cookie("jwt", token, {
-        httpOnly: true,
+        
+        httpOnly: false,
         secure: process.env.NODE_ENV !== "development",
         sameSite: "none",
         maxAge: 30 * 60 * 60 * 24 * 1000
