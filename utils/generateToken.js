5148e0e22544c9b9f63fc443c839dcf16fc4c3f0
unknown <michaelfeleke13@gmail.com>
2023-12-31 11:19:04 -0800

    generate token same site te

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -6,13 +6,13 @@ const generateToken = (res,userId) => {
     })
     ;
 
-    console.log("token generated");
+    console.log(token);
 
     
 
     res.cookie("jwt", token, {
         
-        httpOnly: false,
+        httpOnly: true,
         secure: process.env.NODE_ENV !== "development",
         sameSite: "none",
         maxAge: 30 * 60 * 60 * 24 * 1000
